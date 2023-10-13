c18ff05d3e5a1d50ef11d65c524abd260fbdea54
RSalamatov <salamatov.roman7030@gmail.com>
2023-10-13 21:53:24 +0400

    Исправлен баг при регистрации

--- src/lesson1/Main2.java
+++ src/lesson1/Main2.java
@@ -1,2 +1,21 @@
-package lesson1;public class Ьфшт2 {
+package lesson1;
+
+public class Main2 {
+
+    public static void main(String[] args) {
+        int widthTable = 1;
+        double lengthTable = 1.5;
+        int widthRoom = 3;
+        double lengthRoom = 4.5;
+
+        double sTable = widthTable * lengthTable;
+        double sRoom = widthRoom * lengthRoom;
+        int quantityTable = (int) (sRoom / sTable);
+
+
+
+        System.out.println(sTable);
+        System.out.println(sRoom);
+        System.out.println(quantityTable);
+    }
 }
