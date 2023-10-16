5c7e9a752be7cde13352c8a0643126bb11c57438
RSalamatov <salamatov.roman7030@gmail.com>
2023-10-16 20:58:48 +0400

    Добавлены junit тесты рабочие

--- src/test2/IMatrix.java
+++ src/test2/IMatrix.java
@@ -1,4 +1,4 @@
-package test;
+package test2;
 
 /**
  * Интерфейс работы с матрицами
--- src/test2/MatrixTest.java
+++ src/test2/MatrixTest.java
@@ -1,11 +1,10 @@
-package test;
+package test2;
 
 import junit.framework.TestCase;
-import org.junit.Test;
 
 public class MatrixTest extends TestCase {
 
-    @Test
+
     public void determinantTest1() {
 
         double[][] forMatrix = {{1, 2, 3},
@@ -22,7 +21,7 @@ public class MatrixTest extends TestCase {
     }
 
 
-    @Test
+
     public void determinantTest2() {
 
         double[][] forMatrix = {{-1, -2, -3},
